85c47efd4b335ecad124466b9d2d11cb5374794d
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-08-09 12:54:36 +0600

    changed show local cards to cards from json-server

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,19 +1,48 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles);
+import { getResources } from "../services/requests";
+
+const showMoreStyles = (trigger, wrapper) => {
     const btn = document.querySelector(trigger);
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp');
-    })
+    // cards.forEach(card => {
+    //     card.classList.add('animated', 'fadeInUp');
+    // })
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-        })
+    // btn.addEventListener('click', () => {
+    //     cards.forEach(card => {
+    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    //     })
+    //     btn.remove();
+    // })
+
+    btn.addEventListener("click", () => {
+        getResources("http://localhost:3000/styles")
+            .then((res) => createCards(res))
+            .catch((err) => console.log(err));
         btn.remove();
-    })
+    });
 
-}
+    const createCards = (response) => {
+        response.forEach((item) => {
+            let card = document.createElement('div');
+            card.classList.add(
+                "animated",
+                "fadeInUp",
+                "col-sm-3",
+                "col-sm-offset-0",
+                "col-xs-10",
+                "col-xs-offset-1"
+            );
+
+            card.innerHTML = `
+                <div class=styles-block>
+                    <img src=${item.src} alt>
+                    <h4>${item.title}</h4>
+                    <a href=${item.link}>Подробнее</a>
+                </div>`;
+            document.querySelector(wrapper).appendChild(card);
+        });
+    };
+};
 
 export default showMoreStyles;
