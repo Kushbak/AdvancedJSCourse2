66dd28cda83146b55dc6148db779ad1387dbface
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-08-11 19:25:48 +0600

    called modules, added comments with todo

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
+import scrolling from './modules/scrolling';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -22,5 +25,10 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    // TODO: уменьшить количество переменных для селекторов, рефакторить повторяющиеся добавления обработчиков событий 
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
+    // TODO: Сделать открытие аккордеона только одного элемента, остальным автозакрытие 
+    burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 })
